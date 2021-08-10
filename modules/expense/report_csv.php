4e5ad29085e2bce781671201d5271f95fda78a33
raheelsiddique66 <41738172+raheelsiddique66@users.noreply.github.com>
2021-08-10 20:52:28 +0500

    csv

--- modules/expense/report_csv.php
+++ modules/expense/report_csv.php
@@ -9,7 +9,7 @@ if(numrows($rs)>0){
         $category = get_field($expense_category_id, "expense_category", "title");
         fputcsv($fh,array('Category:', $category));
     }
-    fputcsv($fh,array('S.no','Date/Time','Expense Category','Paid By','Amount','Added By'));
+    fputcsv($fh,array('S.no','Date/Time','Project','Expense Category','Payment Account','Details','Amount','Cheque Number','Added By'));
     $sn=1;
     $total = 0;
     while($r=dofetch($rs)){
@@ -17,9 +17,12 @@ if(numrows($rs)>0){
         fputcsv($fh,array(
             $sn++,
             datetime_convert($r["datetime_added"]),
+            get_field($r["project_id"], "project", "title" ),
             get_field( unslash($r["expense_category_id"]), "expense_category", "title" ),
             get_field( unslash($r["account_id"]), "account", "title" ),
+            unslash($r["details"]),
             curr_format($r["amount"]),
+            unslash($r["cheque_number"]),
             get_field( unslash($r["added_by"]), "admin", "username" )
         ));
     }
--- modules/project_payment/edit.php
+++ modules/project_payment/edit.php
@@ -129,6 +129,16 @@ if(!defined("APP_START")) die("No Direct Access");
             </div>
         </div>
     </div>
+    <div class="form-group">
+    	<div class="row">
+            <div class="col-sm-2 control-label">
+                <label class="form-label" for="correction">Correction</label>
+            </div>
+            <div class="col-sm-10">
+                <input type="text" title="Enter Correction" value="<?php echo $correction; ?>" name="correction" id="correction" class="form-control">
+            </div>
+        </div>
+  	</div>
   	<div class="form-group">
     	<div class="row">
             <div class="col-sm-2 control-label">
